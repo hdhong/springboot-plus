819ff19297c93725b45801655e2054ea0ee69adc
ykb <y@kbblog.cn>
2018-02-24 16:40:55 +0800

    修改菜单和功能点管理的提交接口的方式

--- admin-console/src/main/resources/static/js/admin/menu/menuApi.js
+++ admin-console/src/main/resources/static/js/admin/menu/menuApi.js
@@ -2,15 +2,13 @@
 layui.define([], function(exports) {
 	var api={
 			updateMenu:function(callback){
-				Lib.submitForm($('#updateForm'),{},callback)
+				Lib.submitForm("/admin/menu/update.json",$('#updateForm'),{},callback)
 			},
 			addMenu:function(callback){
-				Lib.submitForm($('#addForm'),{},callback)
+				Lib.submitForm("/admin/menu/save.json",$('#addForm'),{},callback)
 			},
 			del:function(ids,callback){
-				Common.post("/admin/menu/batchDel.json",{"ids":ids},function(){
-					callback();
-				})
+				Common.post("/admin/menu/batchDel.json",{"ids":ids},callback)
 			}
 			
 		
